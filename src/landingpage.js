07b64e26f70ec1b4a8b8c9257b2defac1e76e682
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-01-04 22:34:43 -0500

    landing page updates

--- src/landingpage.js
+++ src/landingpage.js
@@ -7,7 +7,6 @@ import mainImageTwo from '../public/main-images/main-image-two.jpg';
 import galleryPhoto from '../public/main-images/horizonal-img.jpg';
 import verticalGalleryImg from '../public/main-images/vertical-gallery-img.jpg';
 import verticalGalleryImgTwo from '../public/main-images/vertical-gallery-img-two.jpg';
-import horizontalImg from "../public/main-images/horizontal-img.jpg";
 import {Link} from 'react-router-dom';
 import {Player} from 'video-react';
 
@@ -15,19 +14,19 @@ import {Player} from 'video-react';
 export default function LandingPage() {
   return (
       <div className="landing-page">
+
           <div className="content-wrapper">
             <div className="content">
-              <h1 className="h1">Art & Ceramics</h1>
-              <p className="sub-title">Sarah Rondon Gallery Site</p>
+              <h1 className="h1">Sarah Rondon</h1>
+              <p className="sub-title">Sarah Rondon Portfolio Website</p>
             </div>
           </div>
-          <a href="/gallery" className="gallery">
-            <img className="gallery-photo rotate" src={galleryPhoto} alt="art photo" />
-          </a>
+
           <div className="content-wrapper">
             <div className="content">
               <h2 className="gallery-title">Gallery</h2>
               <p className="gallery-content">Photographs of ceramics, painting, and sketch</p>
+
               <div className="content-imgs">
                 <div className="img-container-one">
                   <img src={verticalGalleryImg}></img>
@@ -38,8 +37,22 @@ export default function LandingPage() {
                   <p className="img-title">Collages</p>
                 </div>
               </div>
+
             </div>
           </div>
-      </div>
+
+          <div className="content-wrapper-callout">
+              <div className="left-image">
+                <img src={verticalGalleryImgTwo}></img>
+              </div>
+              <div className="image-card-wrapper">
+                <div className="image-card">
+                  <h2>Sarah Rondon, Artwork</h2>
+                  <p className="image-card-content">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+                </div>
+            </div>
+          </div>
+    </div>
   )
 }
