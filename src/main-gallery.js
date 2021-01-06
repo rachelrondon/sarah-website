eeaf675e83ac65abaad5687e2eab4545c0a92963
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-01-06 13:34:16 -0500

    main gallery update

--- src/main-gallery.js
+++ src/main-gallery.js
@@ -11,28 +11,24 @@ const MainGallery = () => {
             <p>Photographs of ceramics, painting, and mixed-media</p>
 
             <div className="content-imgs">
-                <a href="/oil-paintings" className="oil-paintings-wrapper" target="_blank">
+                <a href="/oil-paintings" className="main-img-card" target="_blank">
                   <img src={verticalGalleryImg}></img>
                   <div className="gallery-txt">
                     <p className="gallery-title">Oil Paintings</p>
                   </div>
                 </a>
-
-              <div className="right-column">
-                <a href="/collages" className="collage-wrapper" target="_blank">
+                <a href="/collages" className="main-img-card" target="_blank">
                   <img src={verticalGalleryImgTwo}></img>
                   <div className="gallery-txt">
                     <p className="gallery-title">Collage</p>
                   </div>
                 </a>
-
-                <a href="/mixed-media" className="collage-wrapper" target="_blank">
+                <a href="/mixed-media" className="main-img-card" target="_blank">
                   <img src={verticalGalleryImgTwo}></img>
                   <div className="gallery-txt">
                     <p className="gallery-title">Mixed Media</p>
                   </div>
                 </a>
-              </div>
             </div>
           </div>
         </div>
