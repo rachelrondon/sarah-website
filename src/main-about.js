5d613dabc72e0b3b39c14083d3f2bbb73999504f
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-01-07 08:53:25 -0500

    update landing page

--- src/main-about.js
+++ src/main-about.js
@@ -5,19 +5,17 @@ import collagePhoto from '../public/gallery-photos/collage-images/always-in-this
 const MainAbout = () => {
     return(
       <div className="main-about">
-        <div className="content-wrapper">
-          <div className="main-about-content">
-            <img src={collagePhoto} alt="blank"></img>
-            <div className="main-about-content-txt">
-              <h2>About the Artist</h2>
-              <p>Ceramics, mixed media, and oil paintings portfolio</p>
-              <p>Ceramics, mixed media, and oil paintings portfolio</p>
-              <p>Ceramics, mixed media, and oil paintings portfolio</p>
-              <p>Ceramics, mixed media, and oil paintings portfolio</p>
-            </div>
+        <img src={collagePhoto} alt="_blank"></img>
+        <div className="main-about-content">
+          <h2>About The Artist</h2>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
+          <p>Sarah Rondon graduated from SUNY New Paltz with a Bachelors of Fine Arts. Sarah specializes in ceramics, mixed media, and oil paintings.</p>
           </div>
         </div>
-      </div>
     )
   }
 
